45f1ba7bdea9c5dd68b23c4b396ea681e0750f17
Naitoshedo <hozuki@naitoshedo.tv>
2025-01-28 11:05:39 -0600

    Revert "Update signalrgb.c"
    This reverts commit a8fdb873bc627982d7ccfc021607f27e8ea93b26.

--- quantum/signalrgb.c
+++ quantum/signalrgb.c
@@ -71,7 +71,7 @@ void led_streaming(uint8_t *data) //Stream data from HID Packets to Keyboard.
       uint8_t  g = data[offset + 1];
       uint8_t  b = data[offset + 2];
 
-      //if ( ((index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock) || ((index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock) || ((index + i) == SCROLL_LOCK_INDEX && host_keyboard_led_state().scroll_lock))   {
+      //if ( ((index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock) || ((index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock) || ((index + i) == SCROLL_LOCK_LED_INDEX && host_keyboard_led_state().scroll_lock))   {
       //if ( ((index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock) || ((index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock))   {
       //if ( (index + i) == CAPS_MAC_WIN_INDEX && host_keyboard_led_state().caps_lock)   {
       //if ( (index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock)   {
